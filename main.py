17bdcbeaa4d00419366f32107c1b1fbb4c62385e
Kenta Kudo <yd37574@gmail.com>
2023-12-02 09:22:41 +0900

    feat(todo): create todo

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from typing import Dict
+import uuid
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +9,23 @@ app = FastAPI()
 @app.get("/")
 def say_hello():
     return {"Hello": "World"}
+
+
+class Todo(BaseModel):
+    id: uuid.UUID
+    name: str
+
+
+todos: Dict[uuid.UUID, Todo] = dict()
+
+
+class CreateTodoRequest(BaseModel):
+    name: str
+
+
+@app.post("/todos")
+def create_todo(request: CreateTodoRequest):
+    id = uuid.uuid4()
+    todo = Todo(id=id, name=request.name)
+    todos[id] = todo
+    return todo
